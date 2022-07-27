e78377f604f506d7dc44f68bf38557cd82823183
dutreras <dutreras369@gmail.com>
2022-07-27 12:02:39 -0400

    connect eureka server

--- currency-conversion-service/src/main/java/cl/paseodelasdelicias/microservices/currencyconversion/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/cl/paseodelasdelicias/microservices/currencyconversion/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package cl.paseodelasdelicias.microservices.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("cl.paseodelasdelicias.microservices.currencyconversion")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
